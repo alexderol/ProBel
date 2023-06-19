c49d5b52f00520299ae7e9f1db676f1e95965a13
Nakamura <erolldemir45@gmail.com>
2023-06-19 12:55:41 +0300

    pom

--- src/test/java/com/abeyis/utilities/Driver.java
+++ src/test/java/com/abeyis/utilities/Driver.java
@@ -3,6 +3,7 @@ package com.abeyis.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import java.util.concurrent.TimeUnit;
 
@@ -39,6 +40,9 @@ public class Driver {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--no-sandbox");
+                    driver = new ChromeDriver(options);
                     driver.manage().window().maximize();
                     //   driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
